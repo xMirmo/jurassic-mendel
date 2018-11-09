47ad7b1094f9619210b2437a56e827028dffc5a2
Andrea Francesco Iuorio <andreafrancesco.iuorio@gmail.com>
2018-11-09 08:02:33 +0100

    Implemented GameState with Active and Pause General refactoring of the event system

--- main.py
+++ main.py
@@ -2,79 +2,61 @@
 import libtcodpy as libtcod
 from collections import deque
 from objects.objects import Player
-from objects.map import Map, MapBuilder, DrawableMap
 
-#should this function always return something? Probably so since a roguelike proceeds only when the player inputs something
-def handle_keys(currentMap, player):
- 
-    key = libtcod.console_wait_for_keypress(True)
-    if key.vk == libtcod.KEY_ESCAPE:
-        return "exit"  #exit game
+from objects.map import *
+from states.states import *
 
-    #movement keys
-    if libtcod.console_is_key_pressed(libtcod.KEY_UP) or chr(key.c) == "k":
-        return (0, -1)
-    elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN) or chr(key.c) == "j":
-        return (0, 1)
-    elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT) or chr(key.c) == "h":
-        return (-1, 0)
-    elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT) or chr(key.c) == "l":
-        return (1, 0)
-    elif chr(key.c) == "y":
-    	return (-1, -1)
-    elif chr(key.c) == "u":
-    	return (1, -1)
-    elif chr(key.c) == "b":
-    	return (-1, 1)
-    elif chr(key.c) == "n":
-    	return (1, 1)
+class Game():
+    def __init__(self):
+        # FIXME this first part should be read from a config file
+        SCREEN_WIDTH = 40
+        SCREEN_HEIGHT = 40
 
-    #catch all the other keys, do nothing
-    else:
-        return (0, 0)
+        libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+        libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'jurassic-mendel', False)
+        libtcod.console_set_default_foreground(0, libtcod.white)
 
+        self.event_queue = deque()
+        self.game_state = ActiveState()
 
-def move_step():
-    #this works on the assumption that there is always an element in the queue, and it's the player movement
-    player_vector = movement_queue.popleft()
+        # FIXME we start with the room, but it could be anything else
+        self.player = Player('@', 5, 5)
 
-    player_new_position = (player.x + player_vector[0], player.y + player_vector[1])
-    if current_map.is_free_at(player_new_position[0], player_new_position[1]):
-        player.move_object(player_vector)
+        self.current_map = MapBuilder().make_map(SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.currentDrawMap = DrawableMap(self.current_map, self.player)
 
+    def enqueue_event(self, eventName, eventData):
+        self.event_queue.append((eventName, eventData))
+    
+    def dequeue_event(self):
+        return self.event_queue.pop()
+    
+    def change_game_state(self, newState):
+        self.game_state = newState
+    
+    def run_game(self):
+        while not libtcod.console_is_window_closed():
+            libtcod.console_clear(0)
+            self.game_state.handle_video(self)
+            libtcod.console_flush()
 
-
-SCREEN_WIDTH = 40
-SCREEN_HEIGHT = 40
-
-libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
-libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'jurassic-mendel', False)
-
-player = Player('@', 5, 5)
-
-
-map_builder = MapBuilder()
-current_map = map_builder.make_map(SCREEN_WIDTH, SCREEN_HEIGHT)
-
-
-currentDrawMap = DrawableMap(current_map, player)
-
-movement_queue = deque()
-libtcod.console_set_default_foreground(0, libtcod.white)
+            key = libtcod.console_wait_for_keypress(True)
+            self.game_state.handle_input(self, key)
+            # handling events
+            while self.event_queue:
+                (eventName, eventData) = self.dequeue_event()
+                # FIXME this is perfect for patter matching
+                if(eventName == "exit_game"):
+                    sys.exit()
+                elif(eventName == "player_movement"):
+                    player_new_position = (self.player.x + eventData[0], self.player.y + eventData[1])
+                    if self.current_map.is_free_at(player_new_position[0], player_new_position[1]):
+                        self.player.move_object(eventData)
+                elif(eventName == "go_pause"):
+                    self.game_state = PauseState()
+                elif(eventName == "go_active"):
+                    self.game_state = ActiveState()
 
 
-while not libtcod.console_is_window_closed():
-    libtcod.console_clear(0)
-    currentDrawMap.draw()
-    player.draw()
-    libtcod.console_flush()
 
-    command = handle_keys(current_map, player)
-
-    if command is "exit":
-        break
-    #the only possible action rn is moving so there is no need to check command
-    else:
-        movement_queue.append(command)
-
-    move_step()
+Game().run_game()
---
+++ states/states.py
@@ -0,0 +1,48 @@
+import libtcodpy as libtcod
+import sys
+
+class GameState():
+    def __init__(self, nameState):
+        self.nameState = nameState
+    
+    def handle_input(self, game, key):
+        pass
+    
+    def handle_video(self, game):
+        pass
+    
+class ActiveState(GameState):
+    def __init__(self):
+        GameState.__init__(self, "Active")
+    
+    def handle_video(self, game):
+        game.currentDrawMap.draw()
+        game.player.draw()
+
+    def handle_input(self, game, key):
+        key_map = {
+            libtcod.KEY_ESCAPE: ("exit_game", None),
+            libtcod.KEY_ENTER: ("go_pause", None),
+
+            libtcod.KEY_UP: ("player_movement", (0, -1)),
+            libtcod.KEY_DOWN: ("player_movement", (0, 1)),
+            libtcod.KEY_LEFT: ("player_movement", (-1, 0)),
+            libtcod.KEY_RIGHT: ("player_movement", (1, 0))
+        }
+
+        (eventName, eventData) = key_map.get(key.vk, ('nop', None))
+        game.enqueue_event(eventName, eventData)
+
+class PauseState(GameState):
+    def __init__(self):
+        GameState.__init__(self, "Pause")
+    
+    def handle_video(self, game):
+        libtcod.console_print_ex(0, 0, 0, libtcod.BKGND_NONE, libtcod.LEFT, "Pause")
+
+    def handle_input(self, game, key):
+        if key.vk == libtcod.KEY_ESCAPE:
+            game.enqueue_event("exit_game", None)
+        if key.vk == libtcod.KEY_ENTER:
+            game.enqueue_event("go_active", None)
+
